00c2921494e922d1c7f2e98cd1f182650a954298
Javid <jaweedguseinov@gmail.com>
2022-10-18 00:21:34 +0300

    Add files via upload

---
+++ Lesson3/src/abstractClasses/WomanGameCalculator.java
@@ -0,0 +1,7 @@
+package abstractClasses;
+
+public class WomanGameCalculator extends GameCalculator {
+	public void calculate() {
+		System.out.println("Puanınız : 95");
+	}
+}
---
+++ Lesson3/src/overriding/StudentCreditManager.java
@@ -0,0 +1,7 @@
+package overriding;
+
+public class StudentCreditManager extends BaseCreditManager {
+	public double Calculate(double amount) {
+		return amount * 1.10;
+	}
+}
